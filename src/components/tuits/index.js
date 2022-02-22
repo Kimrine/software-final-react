ddd0e23ff241fb804431078e3b08d79d53721a88
JoseAnnunziato <jose.annunziato@sonos.com>
2022-02-22 10:52:41 -0500

    a3

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -4,18 +4,18 @@ import Tuit from "./tuit";
 
 function Tuits({tuits = [], deleteTuit}) {
     return (
-        <div>
-            <ul className="ttr-tuits list-group">
-                {
-                  tuits.map && tuits.map(tuit => {
-                        return (
-                            <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
-                        );
-                    })
-                }
-            </ul>
-        </div>
-    );
+    <div>
+      <ul className="ttr-tuits list-group">
+        {
+          tuits.map && tuits.map(tuit => {
+            return (
+              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default Tuits;
