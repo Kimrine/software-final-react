3715f2a7137a8c4593325c2a0a8a4c0c334cdb04
kimrine <347642700@qq.com>
2022-03-24 14:42:40 -0400

    modify

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Tuits from "../tuits";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import * as service from "../../services/auth-service";
 
 const Profile = () => {
