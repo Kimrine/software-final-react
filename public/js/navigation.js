6549af68f824b72e3737d3280573dddc151e583f
kimrine <347642700@qq.com>
2022-03-24 21:08:41 -0400

    modify

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -15,8 +15,9 @@ function Navigation() {
     {label: 'Lists', icon: 'fa-list', path: '/lists'},
     {label: 'Profile', icon: 'fa-user', path: '/profile/mytuits'},
     {label: 'More', icon: 'fa-circle-ellipsis', path: '/more'},
-    {label: 'Login', icon: 'fa-user', path: '/login'},
     {label: 'Signup', icon: 'fa-user', path: '/signup'},
+    {label: 'Login', icon: 'fa-user', path: '/login'},
+
   ]
   return(
       <div className="ttr-navigation">
