8b491d8176c8ce99dd308b90fe2ec42cf7f2f339
kimrine <347642700@qq.com>
2022-03-24 21:14:45 -0400

    modify

--- src/components/profile/signup.js
+++ src/components/profile/signup.js
@@ -1,34 +1,28 @@
-import {useState} from "react";
-import * as service from "../../services/auth-service";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import * as service from "../../services/auth-service"
 
-const Signup = () => {
-    const [newUser, setNewUser] = useState({});
+export const Signup = () => {
+    const [loginUser, setLoginUser] = useState({});
     const navigate = useNavigate();
-    const signup = () =>
-        service.signup(newUser)
-            .then(() => navigate('/home'))
+    const login = () =>
+        service.login(loginUser)
+            .then((user) => navigate('/profile/mytuits'))
             .catch(e => alert(e));
     return (
         <div>
-            <h1>Signup</h1>
+            <h1>Login</h1>
             <input className="mb-2 form-control"
                    onChange={(e) =>
-                       setNewUser({...newUser, username: e.target.value})}
+                       setLoginUser({...loginUser, username: e.target.value})}
                    placeholder="username"/>
             <input className="mb-2 form-control"
                    onChange={(e) =>
-                       setNewUser({...newUser, password: e.target.value})}
+                       setLoginUser({...loginUser, password: e.target.value})}
                    placeholder="password" type="password"/>
-            <input className="mb-2 form-control"
-                   onChange={(e) =>
-                       setNewUser({...newUser, email: e.target.value})}
-                   placeholder="email" type="email"/>
-            <button onClick={signup}
-                    className="btn btn-primary mb-5">Signup
+            <button onClick={login}
+                    className="btn btn-primary mb-5">Login
             </button>
         </div>
     );
 };
-
-export default Signup;
