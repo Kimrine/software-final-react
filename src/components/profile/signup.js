3a6668d5c25167288e47014c5511d5631614506a
kimrine <347642700@qq.com>
2022-03-25 16:00:38 -0400

    add router signup

--- src/components/profile/signup.js
+++ src/components/profile/signup.js
@@ -13,17 +13,21 @@ const Signup = () => {
     return (
         <div>
             <h1>Signup</h1>
-            <input onChange={(e) =>
-                setNewUser({...newUser,
-                               username: e.target.value})}/>
-            <input onChange={(e) =>
-                setNewUser({...newUser,
-                               password: e.target.value})}/>
-            <input onChange={(e) =>
-                setNewUser({...newUser,
-                               email: e.target.value})}/>
-            <button onClick={signup}>
-                Signup</button>
+            <input className="mb-2 form-control"
+                   onChange={(e) =>
+                       setNewUser({...newUser, username: e.target.value})}
+                   placeholder="username"/>
+            <input className="mb-2 form-control"
+                   onChange={(e) =>
+                       setNewUser({...newUser, password: e.target.value})}
+                   placeholder="password" type="password"/>
+            <input className="mb-2 form-control"
+                   onChange={(e) =>
+                       setNewUser({...newUser, email: e.target.value})}
+                   placeholder="email" type="email"/>
+            <button onClick={signup}
+                    className="btn btn-primary mb-5">Signup
+            </button>
         </div>
     );
 }
