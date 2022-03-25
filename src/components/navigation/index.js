9d03c65dd8f1e0e45c909bc7c3ea68335259d7c7
kimrine <347642700@qq.com>
2022-03-24 20:49:08 -0400

    modify

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -16,7 +16,7 @@ function Navigation() {
     {label: 'Profile', icon: 'fa-user', path: '/profile'},
     {label: 'More', icon: 'fa-circle-ellipsis', path: '/more'},
     {label: 'Login', icon: 'fa-user', path: '/login'},
-    {label: 'Signup', icon: 'fa-user', path: '/signup'}
+    {label: 'Signup', icon: 'fa-user', path: '/Signup'}
   ]
   return(
     <div className="ttr-navigation">
