ecf81351a62b923b97c7bc7be3445db7497fbdb2
kimrine <347642700@qq.com>
2022-03-29 19:58:00 -0400

    add unit test for dislike button service

--- src/services/like-service.js
+++ src/services/like-service.js
@@ -7,9 +7,6 @@ const api = axios.create({
                              withCredentials: true
                          });
 
-export const userLikesTuit = (uid, tid) =>
-    api.put(`${USERS_API}/${uid}/likes/${tid}`)
-        .then(response => response.data);
 
 export const userTogglesTuitLikes = (uid,tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
